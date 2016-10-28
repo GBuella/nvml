d2ee58bddee4cde9efb58d73e0c1a23d4038458e
Gabor Buella <gabor.buella@intel.com>
2016-10-28 14:29:01 +0200

    file: routing syscalls getdents and getdents64

--- src/test/file_preload/dummy_prog.c
+++ src/test/file_preload/dummy_prog.c
@@ -34,58 +34,88 @@
  * dummy_prog.c - a dummy prog using pmemfile, checking each return value
  */
 
+#include <err.h>
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <dirent.h>
 
 int
 main(int argc, char **argv)
 {
 	int fd;
+	struct dirent *dir_entry;
+	DIR  *dir_to_list;
 	char buf0[] = "Hello #0 World!\n";
 	char buf1[] = "Hello #1 World!\n";
 
-	if (argc < 4)
+	if (argc < 5)
 		return 1;
 
 	const char *full_path = argv[1];
-	const char *dir = argv[2];
+	const char *chdir_path = argv[2];
 	const char *relative_path = argv[3];
+	const char *dir_to_list_path = argv[4];
 
+	/*
+	 * Creating a file with an absolute path,
+	 * and writing to it.
+	 */
 	if ((fd = open(full_path, O_CREAT | O_RDWR, 0666)) < 0)
-		return 1;
+		err(1, "open(\"%s\", O_CREAT | O_RDWR, 0666) ", full_path);
 
 	if (close(fd) != 0)
-		return 1;
+		err(1, "close \"%s\"", full_path);
 
-	if ((fd = open(full_path, O_WRONLY, 0)) < 0)
-		return 1;
+	if ((fd = open(full_path, O_WRONLY)) < 0)
+		err(1, "open(\"%s\", O_WRONLY) ", full_path);
 
 	if (write(fd, buf0, sizeof(buf0)) != sizeof(buf0))
-		return 1;
+		err(1, "write into \"%s\" ", full_path);
 
 	if (close(fd) != 0)
-		return 1;
+		err(1, "close \"%s\"", full_path);
 
-	if (chdir(dir) != 0)
-		return 1;
+	if (chdir(chdir_path) != 0)
+		err(1, "chdir to \"%s\"", chdir_path);
 
+	/*
+	 * Creating a file with an relative path,
+	 * and writing to it.
+	 */
 	if ((fd = open(relative_path, O_CREAT | O_RDWR, 0666)) < 0)
-		return 1;
+		err(1, "open(\"%s\", O_CREAT | O_RDWR, 0666) ", relative_path);
 
 	if (close(fd) != 0)
-		return 1;
+		err(1, "close \"%s\"", relative_path);
 
 	if ((fd = open(relative_path, O_WRONLY, 0)) < 0)
-		return 1;
+		err(1, "open(\"%s\", O_WRONLY) ", relative_path);
 
 	if (write(fd, buf1, sizeof(buf1)) != sizeof(buf1))
-		return 1;
+		err(1, "write into \"%s\" ", relative_path);
 
 	if (close(fd) != 0)
-		return 1;
+		err(1, "close \"%s\"", relative_path);
+
+	/*
+	 * Listing the entries in a directory, using
+	 * the libc interface.
+	 */
+	if ((dir_to_list = opendir(dir_to_list_path)) == NULL)
+		err(1, "opendir \"%s\"", dir_to_list_path);
+
+	while ((dir_entry = readdir(dir_to_list)) != NULL)
+		puts(dir_entry->d_name);
+
+	if (errno != 0)
+		err(1, "readdir at \"%s\"", dir_to_list_path);
+
+	if (closedir(dir_to_list) != 0)
+		err(1, "closedir at \"%s\"", dir_to_list_path);
 
 	return 0;
 }
